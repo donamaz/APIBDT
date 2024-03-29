d718b81a21024a6e8c52ff289aef7cbb8160fb7d
donamaz <you@example.com>
2024-03-29 16:25:23 +0700

    Add new project food

---
+++ BanDienThoai/Repositories/Interface/IEmail.cs
@@ -0,0 +1,7 @@
+﻿namespace BanDienThoai.Repositories.Interface
+{
+    public interface IEmail
+    {
+        public void Send(string to, string subject, string html, string? from = null);
+    }
+}
