b5ad0b668af384e3faaa67dc15f7b43919cb659a
donamaz <157345298+donamaz@users.noreply.github.com>
2024-04-09 10:15:05 +0700

    Delete BanDienThoai directory

--- BanDienThoai/Repositories/Interface/IDonhang.cs
+++
@@ -1,9 +0,0 @@
-﻿using BanDienThoai.Models;
-
-namespace BanDienThoai.Repositories.Interface
-{
-    public interface IDonhang
-    {
-        public Task<int> AddDonhang(DonhangModel donhangModel, IEnumerable<CTDonhangModel> chitietDonhangModels, KhachhangModel khachhangModel);
-    }
-}
--- BanDienThoai/Repositories/Interface/IEmail.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BanDienThoai.Repositories.Interface
-{
-    public interface IEmail
-    {
-        public void Send(string to, string subject, string html, string? from = null);
-    }
-}
